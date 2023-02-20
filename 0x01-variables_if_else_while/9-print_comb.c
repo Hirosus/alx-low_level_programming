1201f2f07d6cd881b49fa090289d617280c9ebd1
Hirosus <damzvillian@gmail.com>
2023-02-20 13:25:59 -0800

    Huit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- *
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j, k;
+int i;
 
 for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j < 10; j++)
+putchar(i + 48);
+if (i != 9)
 {
-for (k = j + 1; k < 10; k++)
-{
-putchar(i + '0');
 putchar(',');
 putchar(' ');
-putchar(j + '0');
-putchar(',');
-putchar(' ');
-
-if (k == 9)
-putchar(k + '0');
-else
-{
-putchar(k + '0');
+}
+}
 putchar('\n');
-}
-}
-}
-}
+
 return (0);
 }
