8525cbcf5bf7fa4060c7e61a2ce97b8390aeaedc
Hirosus <damzvillian@gmail.com>
2023-02-21 14:10:58 -0800

    ghd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,11 @@
  */
 int print_last_digit(int n)
 {
-int last_digit;
+int last_digit = n % 10;
 
-if (n < 0)
-n = -n;
+if (last_digit < 0)
 
-last_digit = n % 10;
+last_digit *= -1;
 _putchar(last_digit + '0');
 
 return (last_digit);
