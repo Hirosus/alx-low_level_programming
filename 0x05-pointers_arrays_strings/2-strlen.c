d141363866e09cd91255144df9b030aa35acda87
Hirosus <damzvillian@gmail.com>
2023-02-27 17:16:04 -0800

    gh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,9 +11,7 @@ size_t _strlen(const char *str)
 size_t length = 0;
 
 while (*str++)
-{
 length++;
-}
 
 return (length);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,7 @@
 #include "main.h"
-
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
- *
- * Return: void
  */
 void print_rev(char *s)
 {
