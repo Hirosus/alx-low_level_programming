0e83edfdd63be5881f60e0fb6d1eeb79325ae6b6
Hirosus <damzvillian@gmail.com>
2023-05-18 06:53:06 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: a C program that prints exactly and that piece of art is useful
  *
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
 	write(2,
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (0);
+		return (1);
 }
