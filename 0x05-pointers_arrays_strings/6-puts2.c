3ab7fbd490c296547015a3d0206697f555080b0b
Hirosus <damzvillian@gmail.com>
2023-05-29 15:24:56 -0700

    Pointees

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - function should print only one character out of two
+ * @str: string to be printed
+ *
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+	char *j = str;
+	int k;
+
+	while (*j != '\0')
+	{
+		j++;
+		len++;
+	}
+	i = len - 1;
+	for (k = 0; k <= i ; k++)
+	{
+		if (k % 2 == 0)
+		{
+			_putchar(str[k]);
+		}
+	}
+	_putchar('\n');
+}
