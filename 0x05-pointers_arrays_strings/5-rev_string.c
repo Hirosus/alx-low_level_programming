bf673defe2f651c3b8e85461bf5ec5151c2d2c2f
Hirosus <damzvillian@gmail.com>
2023-05-29 14:33:27 -0700

    Hig

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int len = 0;
 	int i;
 
-	while(s[len] != '\0')
+	while (s[len] != '\0')
 		len++;
 	for (i = 0; i < len; i++)
 	{
@@ -20,4 +20,4 @@ void rev_string(char *s)
 		s[i] = s[len];
 		s[len] = rev;
 	}
-}		
+}
