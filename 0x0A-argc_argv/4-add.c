c23d1edac8bae039d6c84a8d2f9f381dc571d9b8
Hirosus <damzvillian@gmail.com>
2023-06-09 19:10:24 -0700

    Ghiy

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,4 +9,24 @@
  * Return: 1 (error), otherwise 0
  */
 
+int main(int argc, char *argv[])
+{
 
+	int i, j, k = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		k += atoi(argv[i]);
+	}
+	printf("%d\n", k);
+
+	return (0);
+}
