23741716cc30a15405ce01787c6a45619b8eff6a
Hirosus <damzvillian@gmail.com>
2023-06-19 18:23:53 -0700

    hu

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - A new type describing a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
