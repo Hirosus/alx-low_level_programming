07ccd3859bda319fb1c8fc7e0b9d490b3178e871
Hirosus <damzvillian@gmail.com>
2023-07-05 13:01:31 -0700

    betty fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
  * delete_nodeint_at_index - function that deletes the node at index
  * @head: a pointer
  * @index: the index of the node that should be deleted
- * 
+ *
  * Return: 1 if it succeeded, -1 if it failed
  */
 
